e104f90b37ad75365147a804271aa7b9dd87eb87
IlanReinstein <ilanreinstein@gmail.com>
2021-11-01 19:30:57 -0400

    testing postgres connection

--- data_load.py
+++ data_load.py
@@ -6,12 +6,16 @@ from airflow.contrib.hooks.aws_hook import AwsHook
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.operators.postgres_operator import PostgresOperator
 from airflow.operators.python_operator import PythonOperator
+from airflow.operators.dummy_operator import DummyOperator
 # from airflow.providers.google.cloud.transfers.gdrive_to_gcs import GoogleDriveToGCSOperator
 
 import sql_queries
 
-cloudsql = PostgresHook("cloudsql")
-print(cloudsql)
+
+def print_postgres_info():
+    cloudsql = PostgresHook("cloudsql")
+    print(cloudsql)
+    print(cloudsql.run("SELECT 1"))
 # def load_data_to_cloudsql(*args, **kwargs):
 #     aws_hook = AwsHook("aws_credentials")
 #     credentials = aws_hook.get_credentials()
@@ -25,10 +29,10 @@ print(cloudsql)
 #     bucket_name='capstone-ir',
 # )
 
-# dag = DAG(
-#     'load_users_purchases',
-#     start_date=datetime.datetime.now()
-# )
+dag = DAG(
+    'postgres_test_conn',
+    start_date=datetime.datetime.now()
+)
 #
 # create_table = PostgresOperator(
 #     task_id="create_table",
@@ -37,6 +41,11 @@ print(cloudsql)
 #     sql=sql_statements.CREATE_USER_PURCHASE_TABLE
 # )
 #
+print_instance = PythonOperator(
+    task_id='testing_conn',
+    python_callable=print_postgres_info,
+    dag=dag
+)
 # copy_task = PythonOperator(
 #     task_id='load_from_gcs_to_cloudsql',
 #     dag=dag,
