194e84367f36d44f39ee8938955098b09bc413e8
IlanReinstein <ilanreinstein@gmail.com>
2021-11-02 18:08:54 -0400

    testing new copy command

--- sql_queries.py
+++ sql_queries.py
@@ -1,21 +1,20 @@
 CREATE_USER_PURCHASE_TABLE = """
-CREATE TABLE IF NOT EXISTS user_purchase (
-    invoice_number varchar(10),
-    stock_code varchar(20),
-    detail varchar(1000),
-    quantity int,
-    invoice_date timestamp,
-    unit_price numeric(8, 3),
-    customer_id int,
-    country varchar(20)
-);
+    CREATE TABLE IF NOT EXISTS user_purchase (
+        invoice_number varchar(10),
+        stock_code varchar(20),
+        detail varchar(1000),
+        quantity int,
+        invoice_date timestamp,
+        unit_price numeric(8, 3),
+        customer_id int,
+        country varchar(20)
+    );
 """
 
 COPY_SQL = """
-COPY {}
-FROM '{}'
-DELIMITER ','
-CSV HEADER;
+    '\copy {} 
+    from '{}'
+    with (format csv, header true, delimiter ',');'
 """
 
 COPY_ALL_USER_PURCHASE_SQL = COPY_SQL.format(
