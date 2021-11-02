f9196490e7f15a0c1cd64caae7ef4e754220cf80
IlanReinstein <ilanreinstein@gmail.com>
2021-11-02 17:45:30 -0400

    adding full operator

--- data_load.py
+++ data_load.py
@@ -11,37 +11,35 @@ from airflow.providers.google.suite.sensors.drive import GoogleDriveFileExistenc
 # from airflow.providers.google.cloud.transfers.gdrive_to_gcs import GoogleDriveToGCSOperator
 
 import sql_queries
-FOLDER_ID = '1rgBurNjLUqErTcaLx0TB53FNWEsausiJhP9Oa3goJSzlv3_xaAjBD8tPz4ROmSIq96KSHggH'
-FILE_NAME = 'user_purchase.csv'#'1nj2AXJG10DTjSjL0J17WlDdZeunJQ9r6'
+FOLDER_ID = '1nj2AXJG10DTjSjL0J17WlDdZeunJQ9r6'#'1rgBurNjLUqErTcaLx0TB53FNWEsausiJhP9Oa3goJSzlv3_xaAjBD8tPz4ROmSIq96KSHggH'
+FILE_NAME = 'user_purchase.csv'#
 
-# def print_postgres_info():
-#     cloudsql = PostgresHook("cloudsql")
-#     print(cloudsql)
-#     return cloudsql.run("SELECT 1")
+
 def load_data_to_cloudsql(*args, **kwargs):
     # aws_hook = AwsHook("aws_credentials")
     # credentials = aws_hook.get_credentials()
     cloudsql_hook = PostgresHook("cloudsql")
     cloudsql_hook.run(sql_queries.COPY_ALL_USER_PURCHASE_SQL)
 
-# upload_gdrive_to_gcs = GoogleDriveToGCSOperator(
-#     task_id="upload_gdrive_object_to_gcs",
-#     folder_id='my-drive',
-#     file_name='user-purchase.csv',
-#     bucket_name='capstone-ir',
-# )
-
 dag = DAG(
     'load_users_table',
     start_date=datetime.datetime.now()
 )
 
-drive_sensor = GoogleDriveFileExistenceSensor(
-        task_id="detect_file",
-        folder_id=FOLDER_ID,
-        file_name=FILE_NAME,
-        dag = dag
-    )
+upload_gdrive_to_gcs = GoogleDriveToGCSOperator(
+    task_id="upload_gdrive_to_gcs",
+    folder_id=FOLDER_ID
+    file_name=FILE_NAME,
+    bucket_name='capstone-ir',
+)
+
+
+# drive_sensor = GoogleDriveFileExistenceSensor(
+#         task_id="detect_file",
+#         folder_id=FOLDER_ID,
+#         file_name=FILE_NAME,
+#         dag = dag
+#     )
 
 create_table = PostgresOperator(
     task_id="create_table",
@@ -50,16 +48,11 @@ create_table = PostgresOperator(
     sql=sql_queries.CREATE_USER_PURCHASE_TABLE
 )
 
-# print_instance = PythonOperator(
-#     task_id='testing_conn',
-#     python_callable=print_postgres_info,
-#     dag=dag
-# )
+
 copy_task = PythonOperator(
     task_id='load_from_gcs_to_cloudsql',
     dag=dag,
     python_callable=load_data_to_cloudsql
 )
-#
-#
-drive_sensor #>> create_table >> copy_task
+
+upload_gdrive_to_gcs >> create_table >> copy_task
