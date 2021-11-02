25de7667c956bde3af456d81227519db734e2929
IlanReinstein <ilanreinstein@gmail.com>
2021-11-01 20:47:32 -0400

    fixing SQL typos

--- sql_queries.py
+++ sql_queries.py
@@ -1,5 +1,5 @@
 CREATE_USER_PURCHASE_TABLE = """
-CREATE TABLE IF NOT EXISTS purchases.users_purchases (
+CREATE TABLE IF NOT EXISTS users_purchases (
     invoice_number varchar(10),
     stock_code varchar(20),
     detail varchar(1000),
